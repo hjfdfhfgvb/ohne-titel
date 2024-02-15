bfa56e1b14d8fef4d1498980264b689cb862007d
hjfdfhfgvb <160117670+hjfdfhfgvb@users.noreply.github.com>
2024-02-15 10:32:29 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-music.playMelody("G F G A - F E D ", 120)
-basic.setLedColor(0x00ff00)
-basic.showIcon(IconNames.Happy, 2000)
-basic.setLedColor(0xff0000)
-basic.turnRgbLedOff()
-basic.showIcon(IconNames.Sad, 2000)
-basic.forever(function () {
-	
-})
+
